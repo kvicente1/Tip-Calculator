0ee1ed57550761af82c833226bbf9b8e0e1e33a1
Karina Vicente <karinavicente@node-j41.pool-1-1.dynamic.totbb.net>
2018-09-14 17:24:19 -0700

    First Commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: AnyObject) { //Function, runs every line underneath until it exits
+        view.endEditing(true)
+    }
+    
+    @IBAction func CalculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0 //bill is a variable that will not change later on that is why is called "let"
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill * tip
+        tipLabel.text = String(format: "$%.2f",tip) //anything inside the paratheses that is a variable change it to whatever it's value is
+        totalLabel.text = String(format: "$%.2f",total)
+    }
+    
 }
 
